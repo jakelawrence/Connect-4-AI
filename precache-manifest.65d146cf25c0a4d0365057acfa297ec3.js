39e12fcba3a01a0052e9acb85d752d2038c60544
Jake Lawrence <jakelawrence@csu.fullerton.edu>
2020-05-22 21:24:59 -0700

    Updates

---
+++ precache-manifest.65d146cf25c0a4d0365057acfa297ec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30fb20e07dd6359ae4e32595d254060",
+    "url": "/Unbeatable-Connect-4-AI/index.html"
+  },
+  {
+    "revision": "9454063d044d33308ac3",
+    "url": "/Unbeatable-Connect-4-AI/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "2c884a42f3db9f44a7f3",
+    "url": "/Unbeatable-Connect-4-AI/static/css/main.b1c13860.chunk.css"
+  },
+  {
+    "revision": "9454063d044d33308ac3",
+    "url": "/Unbeatable-Connect-4-AI/static/js/2.599a8dc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Unbeatable-Connect-4-AI/static/js/2.599a8dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c884a42f3db9f44a7f3",
+    "url": "/Unbeatable-Connect-4-AI/static/js/main.a06208e7.chunk.js"
+  },
+  {
+    "revision": "36440aab28ce6f5a5616",
+    "url": "/Unbeatable-Connect-4-AI/static/js/runtime-main.7420e6d3.js"
+  }
+]);
