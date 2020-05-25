83f24c876cae7c61e0cdb496bd38355cf611d5cf
Jake Lawrence <jakelawrence@csu.fullerton.edu>
2020-05-24 21:51:56 -0700

    Updates

---
+++ precache-manifest.0c22445312bdd081896d15aa69a7ea01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1aaa036ede2b63fb363143e6c53979",
+    "url": "/Connect-4-AI/index.html"
+  },
+  {
+    "revision": "9454063d044d33308ac3",
+    "url": "/Connect-4-AI/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "845a1cd0611c3509783a",
+    "url": "/Connect-4-AI/static/css/main.d8f36012.chunk.css"
+  },
+  {
+    "revision": "9454063d044d33308ac3",
+    "url": "/Connect-4-AI/static/js/2.599a8dc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Connect-4-AI/static/js/2.599a8dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845a1cd0611c3509783a",
+    "url": "/Connect-4-AI/static/js/main.efe07254.chunk.js"
+  },
+  {
+    "revision": "e50214deae06dd1c7751",
+    "url": "/Connect-4-AI/static/js/runtime-main.08c88214.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Unbeatable-Connect-4-AI/precache-manifest.8ed867f8db3f0ac2e4fd48e5eaba4350.js"
+  "/Connect-4-AI/precache-manifest.0c22445312bdd081896d15aa69a7ea01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Unbeatable-Connect-4-AI/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Connect-4-AI/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
