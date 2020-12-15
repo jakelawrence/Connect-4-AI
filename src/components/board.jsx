7a2e4280656c323e939610d998e4e010f3813d23
Jake Lawrence <jakemail15@gmail.com>
2020-12-15 08:40:02 -0800

    Styling Changes

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -11,8 +11,8 @@ const CENTER_COLUMN = Math.floor(COLUMNS / 2);
 
 class Board extends Component {
   state = {
-    //init grid
-    grid: [],
+    //init board
+    board: [],
 
     //for winner message after game over
     winnerColor: "",
@@ -35,13 +35,13 @@ class Board extends Component {
   };
 
   componentDidMount() {
-    const grid = drawBoard();
-    this.setState({ grid });
+    const board = drawBoard();
+    this.setState({ board });
   }
   //for pressing new game
-  resetGrid() {
-    const grid = drawBoard();
-    this.setState({ grid });
+  resetBoard() {
+    const board = drawBoard();
+    this.setState({ board });
     this.setState({ winner: "" });
     this.setState({ gameOver: false });
   }
@@ -57,7 +57,7 @@ class Board extends Component {
         updateDifficulty.medium = "difficultyButton";
         updateDifficulty.hard = "difficultyButton";
         updateDifficulty.depth = 2;
-        this.resetGrid();
+        this.resetBoard();
         break;
       case "medium":
         updateDifficulty.selectedDifficulty = "medium";
@@ -65,7 +65,7 @@ class Board extends Component {
         updateDifficulty.medium = "difficultyButtonSelected";
         updateDifficulty.hard = "difficultyButton";
         updateDifficulty.depth = 3;
-        this.resetGrid();
+        this.resetBoard();
         break;
       default:
         updateDifficulty.selectedDifficulty = "hard";
@@ -73,7 +73,7 @@ class Board extends Component {
         updateDifficulty.medium = "difficultyButton";
         updateDifficulty.hard = "difficultyButtonSelected";
         updateDifficulty.depth = 5;
-        this.resetGrid();
+        this.resetBoard();
         break;
     }
     this.setState({ difficulty: updateDifficulty });
@@ -84,18 +84,18 @@ class Board extends Component {
     //if the game is not over
     if (!this.state.gameOver) {
       //if the column is not completely full of game pieces
-      if (this.state.grid[ROWS - 2][col].piece === 0) {
+      if (this.state.board[ROWS - 2][col].piece === 0) {
         //drop the piece
-        let newGrid = dropPiece(this.state.grid, col, PLAYER);
-        this.setState({ grid: newGrid });
+        let newBoard = dropPiece(this.state.board, col, PLAYER);
+        this.setState({ board: newBoard });
         //check for a four in a row
-        let forWinPlayer = checkForWin(this.state.grid, PLAYER);
+        let forWinPlayer = checkForWin(this.state.board, PLAYER);
         if (forWinPlayer.isWin) {
           //highlight the four in a row with css
-          let finalGrid = highlightWin(forWinPlayer, this.state.grid);
+          let finalBoard = highlightWin(forWinPlayer, this.state.board);
           //set the footer to say the winner
           this.setState({
-            grid: finalGrid,
+            board: finalBoard,
             winner: "YOU WIN!",
             winnerColor: "red",
             gameOver: true,
@@ -105,29 +105,28 @@ class Board extends Component {
 
         //get the score and column returned by the minimax
         let minimaxReturn = minimax(
-          this.state.grid, //game board
+          this.state.board, //game board
           this.state.difficulty.depth, //how many steps the minimax looks ahead
           this.state.difficulty.selectedDifficulty //difficulty selected by user
         );
-        console.log(minimaxReturn);
         // get the row that is corresponding to the column returned by minimax for the placement
-        let rowAI = getNextOpenRow(minimaxReturn.move, this.state.grid);
+        let rowAI = getNextOpenRow(minimaxReturn.move, this.state.board);
         //double check the row isnt full
         if (rowAI < ROWS - 1) {
-          let newGrid = getNewGrid(
+          let newBoard = getNewBoard(
             rowAI,
             minimaxReturn.move,
-            this.state.grid,
+            this.state.board,
             AI
           );
-          this.setState({ grid: newGrid });
+          this.setState({ board: newBoard });
           //check for four in a row
-          let forWinAI = checkForWin(this.state.grid, AI);
+          let forWinAI = checkForWin(this.state.board, AI);
           if (forWinAI.isWin) {
             //highlight four in a row with css
-            let finalGrid = highlightWin(forWinAI, this.state.grid);
+            let finalBoard = highlightWin(forWinAI, this.state.board);
             this.setState({
-              grid: finalGrid,
+              board: finalBoard,
               winner: "AI WINS!",
               winnerColor: "gold",
               gameOver: true,
@@ -140,7 +139,7 @@ class Board extends Component {
   }
 
   render() {
-    const { grid } = this.state;
+    const { board } = this.state;
 
     return (
       <div className="backdrop">
@@ -167,14 +166,14 @@ class Board extends Component {
             </div>
           </div>
           <div className="newGame">
-            <div className="newGameButton" onClick={() => this.resetGrid()}>
+            <div className="newGameButton" onClick={() => this.resetBoard()}>
               New Game
             </div>
           </div>
         </div>
         <div className="main">
-          <div className="grid">
-            {grid.map((row, rowIdx) => {
+          <div className="board">
+            {board.map((row, rowIdx) => {
               return (
                 <div className="row" key={rowIdx}>
                   {row.map((node, nodeIdx) => {
@@ -219,43 +218,40 @@ class Board extends Component {
 }
 
 //drop the piece in the game board
-const dropPiece = (grid, col, turn) => {
-  let row = getNextOpenRow(col, grid);
+const dropPiece = (board, col, turn) => {
+  let row = getNextOpenRow(col, board);
   if (row < ROWS - 1) {
-    let newGrid = getNewGrid(row, col, grid, turn);
-    return newGrid;
+    let newBoard = getNewBoard(row, col, board, turn);
+    return newBoard;
   }
 };
 
 //minimax algorithm searches all possibilites to a certain depth
 //uses alpha beta pruing to speed up the process
 //finds the most effective move based on heuristuc score
-const minimax = (grid, depth, difficulty) => {
+const minimax = (board, depth, difficulty) => {
   let best = {
     move: null,
     score: -Infinity,
   };
   let alpha = -Infinity;
   let beta = Infinity;
-  let scoreCompCount = 0;
   //get all non-filled rows
-  let legalMoves = getValidDrops(grid);
+  let legalMoves = getValidDrops(board);
   //simulate drops for each row that is available
   legalMoves.forEach((col) => {
     //create a deep copy of board to avoid changing the state
-    let gridCopy = JSON.parse(JSON.stringify(grid));
-    let newBoard = dropPiece(gridCopy, col, AI);
+    let boardCopy = JSON.parse(JSON.stringify(board));
+    let newBoard = dropPiece(boardCopy, col, AI);
     //pass to min
     let moveScore = min(newBoard, alpha, beta, 1, depth, difficulty);
-    console.log("Col " + col + ": " + moveScore)
     //find the best score of all the columns
     if (moveScore > best.score) {
-      scoreCompCount++;
       best.score = moveScore;
       best.move = col;
     }
     if (alpha >= beta) {
-      return
+      return;
     }
   });
   if (difficulty === "easy" && !Math.floor(Math.random() * 3)) {
@@ -266,10 +262,10 @@ const minimax = (grid, depth, difficulty) => {
 };
 
 //find the move that benefits the AI the most, which is the largest possible heuristic score
-const max = (grid, alpha, beta, currentDepth, depth, difficulty) => {
+const max = (board, alpha, beta, currentDepth, depth, difficulty) => {
   //if the move leads to a connect 4 or tie game
-  if (isTerminalGrid(grid) || currentDepth === depth) {
-    return evaluate(grid, difficulty);
+  if (isTerminalBoard(board) || currentDepth === depth) {
+    return evaluate(board, difficulty);
   }
   //simulate all possible drops again
   //this is a recursive functiom
@@ -278,13 +274,13 @@ const max = (grid, alpha, beta, currentDepth, depth, difficulty) => {
     //start with lowest possible score, negative infinity
     let score = -Infinity;
     //get all open columns
-    let legalMoves = getValidDrops(grid);
+    let legalMoves = getValidDrops(board);
     //simulate a drop for each possible move
     for (let i = 0; i < legalMoves.length; i++) {
-      //deep copy grid so it doesn't change the current state
+      //deep copy board so it doesn't change the current state
       //we do this because we are just simulating the drops
-      let gridCopy = JSON.parse(JSON.stringify(grid));
-      let newBoard = dropPiece(gridCopy, legalMoves[i], AI);
+      let boardCopy = JSON.parse(JSON.stringify(board));
+      let newBoard = dropPiece(boardCopy, legalMoves[i], AI);
       let moveScore = min(
         newBoard,
         alpha,
@@ -307,11 +303,11 @@ const max = (grid, alpha, beta, currentDepth, depth, difficulty) => {
 };
 
 //find the move that hurts the PLAYER the most, which is the smallest possible heuristic score
-const min = (grid, alpha, beta, currentDepth, depth, difficulty) => {
+const min = (board, alpha, beta, currentDepth, depth, difficulty) => {
   //if the move leads to a connect 4 or tie game
   //or if the minimax has looked as far as it can go with the given depth
-  if (isTerminalGrid(grid) || currentDepth === depth) {
-    return evaluate(grid, difficulty);
+  if (isTerminalBoard(board) || currentDepth === depth) {
+    return evaluate(board, difficulty);
   }
 
   //simulate all possible drops again
@@ -321,13 +317,13 @@ const min = (grid, alpha, beta, currentDepth, depth, difficulty) => {
     //start with highest possible score, infinity
     let score = Infinity;
     //get all open columns
-    let legalMoves = getValidDrops(grid);
+    let legalMoves = getValidDrops(board);
     //simulate a drop for each possible move
     for (let i = 0; i < legalMoves.length; i++) {
-      //deep copy grid so it doesn't change the current state
+      //deep copy board so it doesn't change the current state
       //we do this because we are just simulating the drops
-      let gridCopy = JSON.parse(JSON.stringify(grid));
-      let newBoard = dropPiece(gridCopy, legalMoves[i], PLAYER);
+      let boardCopy = JSON.parse(JSON.stringify(board));
+      let newBoard = dropPiece(boardCopy, legalMoves[i], PLAYER);
       let moveScore = max(
         newBoard,
         alpha,
@@ -355,12 +351,12 @@ const min = (grid, alpha, beta, currentDepth, depth, difficulty) => {
 //if its the PLAYER, we dont want that move, so return negative infinity
 //else, the game is a tie so we return zero
 //if the game has been simulated to the maximum depth
-const evaluate = (grid, difficulty) => {
+const evaluate = (board, difficulty) => {
   let score = 0;
 
-  if (isTerminalGrid(grid)) {
-    let checkPlayer = checkForWin(grid, PLAYER);
-    let checkAI = checkForWin(grid, AI);
+  if (isTerminalBoard(board)) {
+    let checkPlayer = checkForWin(board, PLAYER);
+    let checkAI = checkForWin(board, AI);
     if (checkAI.isWin) {
       score = Infinity;
     } else if (checkPlayer.isWin) {
@@ -370,7 +366,7 @@ const evaluate = (grid, difficulty) => {
     }
   } else {
     for (let r = 0; r < ROWS - 1; r++) {
-      if (grid[r][CENTER_COLUMN].piece === 2) {
+      if (board[r][CENTER_COLUMN].piece === 2) {
         score += 1;
       }
     }
@@ -378,10 +374,10 @@ const evaluate = (grid, difficulty) => {
     for (let r = 0; r < ROWS - 1; r++) {
       for (let c = 0; c < COLUMNS - 3; c++) {
         let window = [
-          grid[r][c],
-          grid[r][c + 1],
-          grid[r][c + 2],
-          grid[r][c + 3],
+          board[r][c],
+          board[r][c + 1],
+          board[r][c + 2],
+          board[r][c + 3],
         ];
         score += evaluateWindow(window, difficulty);
       }
@@ -390,10 +386,10 @@ const evaluate = (grid, difficulty) => {
     for (let c = 0; c < COLUMNS; c++) {
       for (let r = 0; r < ROWS - 4; r++) {
         let window = [
-          grid[r][c],
-          grid[r + 1][c],
-          grid[r + 2][c],
-          grid[r + 3][c],
+          board[r][c],
+          board[r + 1][c],
+          board[r + 2][c],
+          board[r + 3][c],
         ];
         score += evaluateWindow(window, difficulty);
       }
@@ -403,10 +399,10 @@ const evaluate = (grid, difficulty) => {
     for (let c = 0; c < COLUMNS - 3; c++) {
       for (let r = 0; r < ROWS - 4; r++) {
         let window = [
-          grid[r][c],
-          grid[r + 1][c + 1],
-          grid[r + 2][c + 2],
-          grid[r + 3][c + 3],
+          board[r][c],
+          board[r + 1][c + 1],
+          board[r + 2][c + 2],
+          board[r + 3][c + 3],
         ];
         score += evaluateWindow(window, difficulty);
       }
@@ -416,10 +412,10 @@ const evaluate = (grid, difficulty) => {
     for (let c = 0; c < COLUMNS - 3; c++) {
       for (let r = 3; r < ROWS - 1; r++) {
         let window = [
-          grid[r][c],
-          grid[r - 1][c + 1],
-          grid[r - 2][c + 2],
-          grid[r - 3][c + 3],
+          board[r][c],
+          board[r - 1][c + 1],
+          board[r - 2][c + 2],
+          board[r - 3][c + 3],
         ];
         score += evaluateWindow(window, difficulty);
       }
@@ -486,21 +482,21 @@ const evaluateWindow = (window, difficulty) => {
   return score;
 };
 
-const getValidDrops = (grid) => {
+const getValidDrops = (board) => {
   let validRows = [];
   for (let c = 0; c < COLUMNS; c++) {
-    if (grid[ROWS - 2][c].piece === 0) {
+    if (board[ROWS - 2][c].piece === 0) {
       validRows.push(c);
     }
   }
   return validRows;
 };
 
-const isTerminalGrid = (grid) => {
-  let validDrops = getValidDrops(grid);
+const isTerminalBoard = (board) => {
+  let validDrops = getValidDrops(board);
   if (
-    checkForWin(grid, PLAYER).isWin ||
-    checkForWin(grid, AI).isWin ||
+    checkForWin(board, PLAYER).isWin ||
+    checkForWin(board, AI).isWin ||
     validDrops.length === 0
   ) {
     return true;
@@ -509,7 +505,7 @@ const isTerminalGrid = (grid) => {
   }
 };
 
-const checkForWin = (grid, turn) => {
+const checkForWin = (board, turn) => {
   //check horizontal wins
   let fourInARow = {
     isWin: false,
@@ -533,10 +529,10 @@ const checkForWin = (grid, turn) => {
   for (let c = 0; c < COLUMNS - 3; c++) {
     for (let r = 0; r < ROWS - 1; r++) {
       if (
-        grid[r][c].piece === turn &&
-        grid[r][c + 1].piece === turn &&
-        grid[r][c + 2].piece === turn &&
-        grid[r][c + 3].piece === turn
+        board[r][c].piece === turn &&
+        board[r][c + 1].piece === turn &&
+        board[r][c + 2].piece === turn &&
+        board[r][c + 3].piece === turn
       ) {
         fourInARow.one.r = r;
         fourInARow.one.c = c;
@@ -555,10 +551,10 @@ const checkForWin = (grid, turn) => {
   for (let c = 0; c < COLUMNS; c++) {
     for (let r = 0; r < ROWS - 4; r++) {
       if (
-        grid[r][c].piece === turn &&
-        grid[r + 1][c].piece === turn &&
-        grid[r + 2][c].piece === turn &&
-        grid[r + 3][c].piece === turn
+        board[r][c].piece === turn &&
+        board[r + 1][c].piece === turn &&
+        board[r + 2][c].piece === turn &&
+        board[r + 3][c].piece === turn
       ) {
         fourInARow.one.r = r;
         fourInARow.one.c = c;
@@ -577,10 +573,10 @@ const checkForWin = (grid, turn) => {
   for (let c = 0; c < COLUMNS - 3; c++) {
     for (let r = 0; r < ROWS - 4; r++) {
       if (
-        grid[r][c].piece === turn &&
-        grid[r + 1][c + 1].piece === turn &&
-        grid[r + 2][c + 2].piece === turn &&
-        grid[r + 3][c + 3].piece === turn
+        board[r][c].piece === turn &&
+        board[r + 1][c + 1].piece === turn &&
+        board[r + 2][c + 2].piece === turn &&
+        board[r + 3][c + 3].piece === turn
       ) {
         fourInARow.one.r = r;
         fourInARow.one.c = c;
@@ -599,10 +595,10 @@ const checkForWin = (grid, turn) => {
   for (let c = 0; c < COLUMNS - 3; c++) {
     for (let r = 3; r < ROWS - 1; r++) {
       if (
-        grid[r][c].piece === turn &&
-        grid[r - 1][c + 1].piece === turn &&
-        grid[r - 2][c + 2].piece === turn &&
-        grid[r - 3][c + 3].piece === turn
+        board[r][c].piece === turn &&
+        board[r - 1][c + 1].piece === turn &&
+        board[r - 2][c + 2].piece === turn &&
+        board[r - 3][c + 3].piece === turn
       ) {
         fourInARow.one.r = r;
         fourInARow.one.c = c;
@@ -620,45 +616,45 @@ const checkForWin = (grid, turn) => {
   return fourInARow;
 };
 
-const highlightWin = (fourInARow, grid) => {
-  grid[fourInARow.one.r][fourInARow.one.c].isWinningPiece = true;
-  grid[fourInARow.two.r][fourInARow.two.c].isWinningPiece = true;
-  grid[fourInARow.three.r][fourInARow.three.c].isWinningPiece = true;
-  grid[fourInARow.four.r][fourInARow.four.c].isWinningPiece = true;
-  return grid;
+const highlightWin = (fourInARow, board) => {
+  board[fourInARow.one.r][fourInARow.one.c].isWinningPiece = true;
+  board[fourInARow.two.r][fourInARow.two.c].isWinningPiece = true;
+  board[fourInARow.three.r][fourInARow.three.c].isWinningPiece = true;
+  board[fourInARow.four.r][fourInARow.four.c].isWinningPiece = true;
+  return board;
 };
 
-const getNextOpenRow = (col, grid) => {
+const getNextOpenRow = (col, board) => {
   for (let row = 0; row < ROWS - 1; row++) {
-    if (grid[row][col].piece === 0) {
+    if (board[row][col].piece === 0) {
       return row;
     }
   }
   return Infinity;
 };
 
-const getNewGrid = (row, col, grid, turn) => {
-  grid[row][col].piece = turn;
+const getNewBoard = (row, col, board, turn) => {
+  board[row][col].piece = turn;
   if (turn === PLAYER) {
-    grid[row][col].isPlayer = true;
+    board[row][col].isPlayer = true;
   } else {
-    grid[row][col].isAI = true;
+    board[row][col].isAI = true;
   }
-  grid[row][col].isEmpty = false;
+  board[row][col].isEmpty = false;
 
-  return grid;
+  return board;
 };
 
 const drawBoard = () => {
-  const grid = [];
+  const board = [];
   for (let row = 0; row < ROWS; row++) {
     const currentRow = [];
     for (let col = 0; col < COLUMNS; col++) {
       currentRow.push(createNode(col, row, 0));
     }
-    grid.push(currentRow);
+    board.push(currentRow);
   }
-  return grid;
+  return board;
 };
 
 const createNode = (col, row, piece) => {
